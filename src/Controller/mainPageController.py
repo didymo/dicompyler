39227528b23aebc94d246f60a9930f4bb7a447be
Helena <helena.ibro@gmail.com>
2019-09-04 16:34:06 +1000

    devided the initialisation of the main page window

--- src/Controller/mainPageController.py
+++ src/Controller/mainPageController.py
@@ -7,11 +7,13 @@ from src.Model.LoadPatients import *
 
 class MainPage(QtWidgets.QMainWindow):
     def __init__(self):
-        path = 'dicom_sample'
-        dataset = get_datasets(path)
-        pixmaps = get_pixmaps(dataset)
+        self.path = 'dicom_sample'
+        self.dataset = get_datasets(self.path)
+        self.pixmaps = get_pixmaps(self.dataset)
 
+
+    def displayMainPage(self):
         QtWidgets.QWidget.__init__(self)
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self, pixmaps, path)
+        self.ui.setupUi(self, self.pixmaps, self.path)
 
